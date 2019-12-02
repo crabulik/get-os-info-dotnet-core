c1a7f5d3c057de4595c6dc6cfbbc415e2795b541
Pavel Shylan <pavel.shylan@materialise.kiev.ua>
2019-12-02 18:28:47 +0200

    Big release!

---
+++ GetOsInfo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GetOsInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Alt.NET!");
+            Console.WriteLine($"I'm running on {System.Runtime.InteropServices.RuntimeInformation.OSDescription}");
+        }
+    }
+}
